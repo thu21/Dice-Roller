bee8838ee5ad7cef06d6bf3dac9d16a080ae180a
thu21 <thutrinhvn21@gmail.com>
2021-04-07 17:47:45 +0700

    Learn Kotlin

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.diceroller
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+
+    }
+}
